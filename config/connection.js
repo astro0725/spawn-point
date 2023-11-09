3bc3956a4b7b937ded1519e8fcb6852884c49143
PaBoba <140555335+PaBoba@users.noreply.github.com>
2023-11-08 16:48:05 -0800

    added in the config folder with connection.js

---
+++ config/connection.js
@@ -0,0 +1,23 @@
+// Purpose: Establishes connection to database
+const Sequelize = require("sequelize");
+// require the dotenv package and invoke the config() method to load the variables from the .env file into process.env
+require("dotenv").config();
+// use the Sequelize constructor to create a connection to the database
+let sequelize;
+// if the NODE_ENV environment variable is equal to 'production', we will use the JAWSDB_URL environment variable to connect to the JawsDB database
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PW,
+    {
+      host: "localhost",
+      dialect: "mysql",
+      port: 3306,
+    }
+  );
+}
+// export the connection
+module.exports = sequelize;
