4bcd8f2d2331b278f7d30a5ca38a8c6d1bffab60
astro <angelica.strong0725@gmail.com>
2023-11-12 18:51:59 -0800

    implemented logic for creating posts

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,5 +1,16 @@
 // require express router and the user routes
 const router = require("express").Router();
+const { Post } = require("../models");
+
+router.post('/create', async (req, res) => {
+    try {
+        const newPost = await Post.create({
+            ...req.body,
+            userId: req.session.UserId
+        });
+    } catch (error) {
+        res.status(500).json({ message: 'Error creating post', error: error });
+    }
 
 // export router
 module.exports = router;
