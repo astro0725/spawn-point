73a0d53c630a0076f9a78a735da03e72fa48c6a1
astro <angelica.strong0725@gmail.com>
2023-11-12 20:05:50 -0800

    updated post logic and route definition

--- controllers/posts.js
+++ controllers/posts.js
@@ -2,10 +2,17 @@ const { Post } = require("../models");
 
 const createPost = async (req, res) => {
     try {
+        const userId = req.session.userId || req.user.id; // conjecture -- placeholder until i see randy's file structure
+        const { title, body } = req.body;
+        const imageUrl = req.files['image'] ? req.files['image'][0].path : null;
+        const videoUrl = req.files['video'] ? req.files['video'][0].path : null;
         const newPost = await Post.create({
-            ...req.body,
-            userId: req.session.UserId // alleged placeholder
-        });
+            title,
+            body,
+            imageUrl,
+            videoUrl,
+            userId: userId 
+            });
         res.json(newPost);
     } catch (error) {
         res.status(500).json({ message: 'Error creating post', error: error });
