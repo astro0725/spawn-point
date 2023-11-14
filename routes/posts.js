cb161993a770b5fc87c69d23936ed2c9b884e3f1
astro <angelica.strong0725@gmail.com>
2023-11-13 19:11:02 -0800

    added definition to file uploads

--- routes/posts.js
+++ routes/posts.js
@@ -4,6 +4,14 @@ const { upload, createPost, deletePost } = require('../controllers/posts');
 
 router.post('/create', upload.fields([{ name: 'image', maxCount: 3 }, { name: 'video', maxCount: 1 }]), createPost);
 
+app.post('/upload', upload.array('media'), function (req, res, next) {
+    const fileInfos = req.files.map(file => ({
+        url: '/uploads/' + file.filename,
+    }));
+
+    res.json(fileInfos);
+});
+
 router.delete('/delete/:id', deletePost);
 
 module.exports = router;
