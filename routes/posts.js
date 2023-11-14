ca4167b2d29cd38b8c070c72deaafc89cf50e0c1
astro <angelica.strong0725@gmail.com>
2023-11-14 14:59:33 -0800

    added validation and sanitation reqs for posts

--- routes/posts.js
+++ routes/posts.js
@@ -1,8 +1,22 @@
 const express = require('express');
+const { body, validationResult } = require('express-validator');
 const router = express.Router();
 const { upload, createPost, deletePost } = require('../controllers/posts');
 
-router.post('/create', upload.fields([{ name: 'image', maxCount: 3 }, { name: 'video', maxCount: 1 }]), createPost);
+upload.fields([{ name: 'image', maxCount: 3 }, { name: 'video', maxCount: 1 }]),
+    [
+        body('body').trim().isLength({ min: 1 }).withMessage('Post body is required'),
+        body('body').escape(),
+    ],
+    (req, res, next) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+        next();
+    },
+    createPost
+);
 
 app.post('/upload', upload.array('media'), function (req, res, next) {
     const fileInfos = req.files.map(file => ({
