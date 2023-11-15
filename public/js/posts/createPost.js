48833c13c662b170314b781a15b3ae197f835d8a
astro <angelica.strong0725@gmail.com>
2023-11-14 16:36:45 -0800

    updated for firebase uid correction

--- public/js/posts/createPost.js
+++ public/js/posts/createPost.js
@@ -16,7 +16,7 @@ async function submitPost(event) {
         };
 
         // Send a POST request to create the new post
-        fetch('/create', {
+fetch('/create', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
@@ -24,30 +24,27 @@ async function submitPost(event) {
         },
         body: JSON.stringify(postData)
         })
-        .then(response => response.json())
-        .then(data => {
+    .then(response => response.json())
+    .then(data => {
         console.log(data);
         toggleModal('createPostModal'); 
         })
-        .catch((error) => {
+    .catch((error) => {
         console.error('Error:', error);
         });
-    }
+}
 
     // Function to get the current user's authentication status
-    // TODO: update for firebase user ID
-    function getCurrentUser() {
-        return new Promise((resolve, reject) => {
+function getCurrentUser() {
+    return new Promise((resolve, reject) => {
         firebase.auth().onAuthStateChanged((user) => {
             if (user) {
-            user.getIdToken().then(function(token) {
-                resolve({ uid: user.uid, token: token });
-            });
+                resolve(user.uid);
             } else {
-            resolve(null);
-            }
+                resolve(null);
+        }
         });
-        });
+    });
 }
 // add an event listener to the form submission
 document.getElementById('postForm').addEventListener('submit', submitPost);
