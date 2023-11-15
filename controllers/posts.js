ab7d26e3b57605105fdfa4a6e6f52a1f8bc93769
astro <angelica.strong0725@gmail.com>
2023-11-14 19:39:31 -0800

    updated middleware dependencies in routes

--- controllers/posts.js
+++ controllers/posts.js
@@ -72,7 +72,7 @@ const createPost = async (req, res) => {
 const deletePost = async (req, res) => {
     try {
         const postId = req.params.id;
-        const firebaseUserId = req.user.id; // Assuming req.user is set by your authentication middleware
+        const firebaseUserId = req.firebaseUserId 
 
         // First, find the post to check if it exists and belongs to the user
         const post = await Post.findOne({
