10a41bd32da9f68eca03a14914ecba85413b1894
BigDripDarry <darrelsaephan@gmail.com>
2023-11-14 21:13:40 -0800

    added unfollow route function

--- controllers/follow.js
+++ controllers/follow.js
@@ -3,26 +3,32 @@ const User = require("../models/user");
 // route to follow a user
 const followUser = async (req, res) => {
   try {
-    // logic for finding user with uuid that we wanna update for following
-    const { uuid } = req.params;
-    const userToFollow = await User.findOne({ uuid });
-    // checks if user exists
-    if (!userToFollow) {
-      return res.status(404).json({ message: "User not found" });
-    }
-    userToFollow.followers.push(req.user.id); // followers is just conjection for now
-    await userToFollow.save();
+    const followerId = req.user.id; // if we have authentication middleware
+    const followedUserId = req.params.userId;
 
-    // updates users following list, conjecture for now
-    const follower = await User.findbyId(req.user.id);
-    if (!follower) {
-      return res.status(404).json({ message: "Follower not found" });
-    }
-    follower.following.push(userToFollow.id);
-    await follower.save();
+    // checks if user is following
+    const existingFollower = await Follower.findOne({
+      where: {
+        followerId: followerId,
+        followedUserId: followedUserId,
+      },
+    });
 
-    res.json({ message: "Successfully followed user", user: userToFollow });
+    if (existingFollower) {
+      return res.status(400).json({ message: "User already being followed" });
+    }
+
+    //creates a follow
+    const newFollower = await Follower.create({
+      followerId: followerId,
+      followedUserId: followedUserId,
+    });
+
+    res
+      .status(200)
+      .json({ message: "Successfully followed user", dara: newFollower });
   } catch (error) {
+    console.log(error);
     res.status(500).json({ message: "Error following user", error: error });
   }
 };
@@ -31,7 +37,25 @@ const followUser = async (req, res) => {
 const unfollowUser = async (req, res) => {
   try {
     // logic for finding user, then remove corresponding user that we wanna unfollow
+    const { uuid } = req.params;
+    const userToUnfollow = await User.findOne({ where: { uuid } });
+    // checks if user exists
+    if (!userToUnfollow) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    // finds the follower
+    const follower = await User.findByPk(req.user.id);
+
+    if (!follower) {
+      return res.status(404).json({ message: "Follower not found" });
+    }
+
+    // removes the userToUnfollow's ID from the follower's following list
+    await follower.removeFollowing(userToUnfollow);
+
+    res.json({ message: "Succcessfully unffollowed user" });
   } catch (error) {
+    console.log(error);
     res.status(500).json({ message: "Error unfollowing user", error: error });
   }
 };
