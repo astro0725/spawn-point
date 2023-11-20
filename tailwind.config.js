a7033f7ee5080d484bbd2b91ebab28c435eb7faf
astro <angelica.strong0725@gmail.com>
2023-11-20 13:41:32 -0800

    tailwind fix -- header still broken

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ['./views/partials/*.handlebars', './views/*.handlebars'],
   theme: {
     extend: {
       fontFamily: {
@@ -8,7 +8,6 @@ module.exports = {
       },
       colors: {
         "bg-dark": "#21262a",
-        "bg-light": "#ffffff",
         "light-blue": "#a5dbff",
         purple: "#7d1399",
         "neon-pink": "#eb7dff",
