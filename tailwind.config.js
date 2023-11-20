245806ff2b0ca1d15b6fa81bf22f9c1348bcef59
astro <angelica.strong0725@gmail.com>
2023-11-20 10:48:51 -0800

    tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        "bg-dark": "#21262a",
+        "bg-light": "#ffffff",
+        "light-blue": "#a5dbff",
+        purple: "#7d1399",
+        "neon-pink": "#eb7dff",
+        pink: "#ffa7bb",
+        yellow: "#ffe400",
+        cream: "#fde8da",
+      }
+    },
+  },
+  plugins: [],
+}
+
