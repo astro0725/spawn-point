93d710509c274c10c065f0160828fc548fb1df6c
astro <angelica.strong0725@gmail.com>
2023-11-20 21:18:47 -0800

    search bar view fixed

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,7 @@ module.exports = {
         'press-start':['"Press Start 2P"']
       },
       colors: {
-        "dark": "#21262a",
+        "dark-bg": "#21262a",
         "dark-purple": "#512a8e",
         "light-blue": "#a5dbff",
         purple: "#7d1399",
@@ -15,9 +15,8 @@ module.exports = {
         pink: "#ffa7bb",
         yellow: "#ffe400",
         cream: "#fde8da",
-      }
+      },
     },
   },
   plugins: [],
 }
-
