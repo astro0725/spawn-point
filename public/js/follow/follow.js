a91c914f0240e97c73936921d22840910ae60f5a
BigDripDarry <darrelsaephan@gmail.com>
2023-11-20 19:55:45 -0800

    added JS for functionality of follow button on frontend

---
+++ public/js/follow/follow.js
@@ -0,0 +1,73 @@
+// firebase config
+document.addEventListener("DOMContentLoaded", function () {
+  const firebaseConfig = {
+    apiKey: "AIzaSyCiZF1VwaJ8d18jp6nJtC2AQnboz1AerYg",
+    authDomain: "pixel-pals-fdb7e.firebaseapp.com",
+    projectId: "pixel-pals-fdb7e",
+    storageBucket: "pixel-pals-fdb7e.appspot.com",
+    messagingSenderId: "308630756272",
+    addId: "1:308630756272:web:1f7f9e0703c0803b449475",
+  };
+
+  // initiializes firebase
+  firebaseConfig.initializeApp(firebaseConfig);
+  // function to follow user
+  async function followUser(username) {
+    try {
+      // gets user from firebase auth
+      const user = firebase.auth().currentUser;
+      // checks if user is singed in
+      if (!user) {
+        throw new Error("User not signed");
+      }
+      // get user's following list
+      let following = user.displayName ? user.displayName.split(",") : [];
+
+      // check if user is already following
+      if (following.includes(username)) {
+        console.log("You are already following this user!");
+        return;
+      }
+      // add username to list
+      following.push(username);
+      // preps data for PUTmrequest to update user profile
+      const response = await fetch({
+        method: "POST",
+        headers: {
+          "Content-Type": "route",
+        },
+        body: JSON.stringify({
+          // gets ID token for auth
+          idToken: await user.getIdToken(),
+          // updates displayName with new following list
+          displayName: following.join(","),
+          // Ensure a new ID token is returned
+          returnSecureToken: true,
+        }),
+      });
+
+      // parse the respone JSON
+      const data = await response.json();
+
+      // checks if request was successful
+      if (!response.ok) {
+        throw new Error("Failed to update user profile: ${data.error.message}");
+      }
+      console.log("User has been followed successfully!");
+    } catch (error) {
+      // handles if error
+      console.error("Error following user:", error);
+    }
+  }
+
+  // attaches event listener to follow button
+  const followButton = document.getElementById("followButton");
+  followButton.addEventListener("click", function () {
+    // prompts user to enter username that they want to follow
+    const userToFollow = prompt("Enter the user you want to follow:");
+    if (userToFollow) {
+      // calls followUser with entered username
+      followUser(userToFollow);
+    }
+  });
+});
