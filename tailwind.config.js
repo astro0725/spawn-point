ace8087ab6cc780196e98d205ef1e23dba0260f1
astro <angelica.strong0725@gmail.com>
2023-11-20 22:07:55 -0800

    Revert "search bar view fixed"
    This reverts commit 93d710509c274c10c065f0160828fc548fb1df6c.

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,7 @@ module.exports = {
         'press-start':['"Press Start 2P"']
       },
       colors: {
-        "dark-bg": "#21262a",
+        "dark": "#21262a",
         "dark-purple": "#512a8e",
         "light-blue": "#a5dbff",
         purple: "#7d1399",
@@ -15,8 +15,9 @@ module.exports = {
         pink: "#ffa7bb",
         yellow: "#ffe400",
         cream: "#fde8da",
-      },
+      }
     },
   },
   plugins: [],
 }
+
