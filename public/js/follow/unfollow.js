e8e982ec3df3a913e8dd922656a24a01e92721f2
BigDripDarry <darrelsaephan@gmail.com>
2023-11-20 20:49:29 -0800

    fixed problems found in unfollow funct

--- public/js/follow/unfollow.js
+++ public/js/follow/unfollow.js
@@ -15,55 +15,55 @@ document.addEventListener("DOMContentLoaded", function () {
   // function to unfollow user
   async function unfollowUser(username) {
     try {
-        // gets user from firebase auth
-        const user = firebase.auth(). currentUser;
-        // checks if user is signed in
-        if(!user){
-            throw new Error('User not signed in!');
-        }
-        // gets user following list
-        let following = user.displayName ? user.displayName.split(',') : [];
-        // checks if user is already following
-        if(!following.includes.(username)) {
-            console.log('You are not following this user!');
-            return;
-        }
+      // gets user from firebase auth
+      const user = firebase.auth().currentUser;
+      // checks if user is signed in
+      if (!user) {
+        throw new Error("User not signed in!");
+      }
+      // gets user following list
+      let following = user.displayName ? user.displayName.split(",") : [];
+      // checks if user is already following
+      if (!following.includes(username)) {
+        console.log("You are not following this user!");
+        return;
+      }
 
-        // preps data for PUT request to update user profile
-        const response = await fetch('fetchRouteNeeded', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'route'
-            },
-            body: JSON.stringify({
-                //gets ID token for auth
-                idToken: await user.getIdToken(),
-                // updates displayName with new following list
-                displayName: following.join(','),
-                // ensure that a new id token is returned
-                returnSecureToken: true,
-            }),
+      // preps data for PUT request to update user profile
+      const response = await fetch("fetchRouteNeeded", {
+        method: "POST",
+        headers: {
+          "Content-Type": "route",
+        },
+        body: JSON.stringify({
+          //gets ID token for auth
+          idToken: await user.getIdToken(),
+          // updates displayName with new following list
+          displayName: following.join(","),
+          // ensure that a new id token is returned
+          returnSecureToken: true,
         }),
+      });
 
-        // parse response JSON
-        const data = await response.json();
+      // parse response JSON
+      const data = await response.json();
 
-        // checks if request was successful
-        if(!response.ok) {
-            throw new Error(`Failed to update user profile: ${data.error.message}`);
-        }
-        console.log('User has been unfollowed successfully!');
+      // checks if request was successful
+      if (!response.ok) {
+        throw new Error(`Failed to update user profile: ${data.error.message}`);
+      }
+      console.log("User has been unfollowed successfully!");
     } catch (error) {
-        // handles errors
-        console.error('Error unfollowing user:', error);
+      // handles errors
+      console.error("Error unfollowing user:", error);
     }
   }
 
   // attaches event listenter to unfollow button
-  const unfollowButton = document.getElementById('unfollowButton');
-  unfollowButton.addEventListener('click', function() {
+  const unfollowButton = document.getElementById("unfollowButton");
+  unfollowButton.addEventListener("click", function () {
     // gets username from the profile
-    const userToUnfollow = document.getElementById('username').innerText;
+    const userToUnfollow = document.getElementById("username").innerText;
     // call unfollowUser funct with username
     unfollowUser(userToUnfollow);
   });
