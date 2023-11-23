3024fff1c5bde8f2415394d8cd0c56d34d77861b
astro <angelica.strong0725@gmail.com>
2023-11-23 12:19:06 -0800

    mysql now working

--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,17 @@
--- Drop the database if it exists and then create it
-DROP DATABASE IF EXISTS pixelpals_development;
-CREATE DATABASE pixelpals_development;
+-- Creates database if it doesn't exist
+CREATE DATABASE IF NOT EXISTS pixelpals_development;
 
 -- Select the newly created database
 USE pixelpals_development;
+
+CREATE TABLE IF NOT EXISTS Users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    firebaseUserId VARCHAR(255) NOT NULL UNIQUE,
+    username VARCHAR(255) NOT NULL UNIQUE,
+    createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+-- CREATE DATABASE IF NOT EXISTS pixelpals_development;
+
+-- USE pixelpals_test;
