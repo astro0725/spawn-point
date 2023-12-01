f14cb5a56450477878f285069139077667c8f218
astro <angelica.strong0725@gmail.com>
2023-12-01 10:48:01 -0800

    left menu styles added

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,21 @@ module.exports = {
   content: ["./views/partials/*.handlebars", "./views/*.handlebars"],
   theme: {
     extend: {
+      spacing: {
+        '7.5': '30px',
+      },
+      width: {
+        '260': '260px',
+      },
       fontFamily: {
         "press-start": ['"Press Start 2P"'],
+        "dm-sans": ['"DM Sans"', 'sans-serif'],
+        "source-sans": ['"Source Sans Pro"', 'sans-serif'],
       },
       colors: {
+        acf: "#9c9cab",
+        side: "#5c5e6e",
+        bcc: "#272a3a",
         dark: "#21262a",
         "dark-purple": "#512a8e",
         "light-blue": "#a5dbff",
