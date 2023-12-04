24aff6b8f2e0e4fc5426bd4064dd447e02bc5871
astro <angelica.strong0725@gmail.com>
2023-12-04 14:02:42 -0800

    implemented firebaseAuth to protect follow functions from non-authorized users

--- routes/follow.js
+++ routes/follow.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const followController = require('../controllers/follow');
+const firebaseAuth = require('../utils/firebaseAuth');
 
-router.post('/follow/:firebaseUserId', followController.followUser);
-router.delete('/follow/:firebaseUserId', followController.unfollowUser);
-router.get('/friends', followController.getFriends);
+router.post('/follow/:firebaseUserId', firebaseAuth, followController.followUser);
+router.delete('/follow/:firebaseUserId', firebaseAuth, followController.unfollowUser);
+router.get('/friends', firebaseAuth, followController.getFriends);
 
 module.exports = router;
