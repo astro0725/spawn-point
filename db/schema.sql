a94b05a54780c3bccd035fa4bc0e6ede405eadea
astro <angelica.strong0725@gmail.com>
2023-12-04 16:25:54 -0800

    updated schema to reflect structure of my current models

--- db/schema.sql
+++ db/schema.sql
@@ -9,16 +9,14 @@ CREATE TABLE IF NOT EXISTS Users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     firebaseUserId VARCHAR(255) NOT NULL UNIQUE,
     username VARCHAR(255) NOT NULL UNIQUE,
+    biography VARCHAR(255),
+    profilePicture VARCHAR(255),
+    profileHeader VARCHAR(255) NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    previousPasswordHash VARCHAR(255),
     createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
-
--- Alter the Users table to add additional columns
-ALTER TABLE Users
-ADD COLUMN biography VARCHAR(255),
-ADD COLUMN profilePicture VARCHAR(255),
-ADD COLUMN profileHeader VARCHAR(255) NOT NULL,
-ADD COLUMN name VARCHAR(255) NOT NULL;
 
 -- Create the Posts table
 CREATE TABLE IF NOT EXISTS Posts (
@@ -34,6 +32,7 @@ CREATE TABLE IF NOT EXISTS Posts (
 -- Create the Showcase table
 CREATE TABLE IF NOT EXISTS Showcase (
     id INT AUTO_INCREMENT PRIMARY KEY,
+    firebaseUserId VARCHAR(255) NOT NULL,
     createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     FOREIGN KEY (firebaseUserId) REFERENCES Users(firebaseUserId)
@@ -53,11 +52,24 @@ CREATE TABLE IF NOT EXISTS ShowcaseGame (
     showcaseId INT NOT NULL,
     rawgGameId INT NOT NULL,
     PRIMARY KEY (showcaseId, rawgGameId),
-    FOREIGN KEY (showcaseId) REFERENCES Showcase(id),
-    FOREIGN KEY (rawgGameId) REFERENCES RAWGGame(id)
+    FOREIGN KEY (showcaseId) REFERENCES Showcase(firebaseUserId),
+    FOREIGN KEY (rawgGameId) REFERENCES RAWGGame(firebaseUserId)
 );
 
+-- Create the Follow table
+CREATE TABLE IF NOT EXISTS Follow (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    followerId INT NOT NULL,
+    followingId INT NOT NULL,
+    FOREIGN KEY (followerId) REFERENCES Users(firebaseUserId),
+    FOREIGN KEY (followingId) REFERENCES Users(firebaseUserId)
+);
 
--- CREATE DATABASE IF NOT EXISTS spawnpoint_test;
-
--- USE spawnpoint_test;
+-- Create the BlockedUser table
+CREATE TABLE IF NOT EXISTS BlockedUser (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    blockerId INT NOT NULL,
+    blockedId INT NOT NULL,
+    FOREIGN KEY (blockerId) REFERENCES Users(firebaseUserId),
+    FOREIGN KEY (blockedId) REFERENCES Users(firebaseUserId)
+);
